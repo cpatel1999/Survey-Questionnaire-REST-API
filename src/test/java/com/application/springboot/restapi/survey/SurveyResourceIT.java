9968fdd9d18fb2240bb808ef5fd127cd9c3787cc
Charit_Patel <charit15499@gmail.com>
2022-11-21 01:16:44 -0500

    Integration Test for GET method returning List

--- src/test/java/com/application/springboot/restapi/survey/SurveyResourceIT.java
+++ src/test/java/com/application/springboot/restapi/survey/SurveyResourceIT.java
@@ -1,18 +1,28 @@
 package com.application.springboot.restapi.survey;
 
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 
-import java.sql.SQLOutput;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 // Used to listen at random port.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SurveyResourceIT {
 
-    // http://localhost:8080/surveys/Survey1/questions/Question1
+    private static String SPECIFIC_QUESTION_URL = "/surveys/Survey1/questions/Question1";
+    private static String GENERIC_QUESTIONS_URL = "/surveys/Survey1/questions";
+
+    //Used so that application directly talks with https://localhost:RANDOM_PORT
+    @Autowired
+    private TestRestTemplate template;
+
+
     String str = """
             {
                 "id": "Question1",
@@ -27,17 +37,49 @@ public class SurveyResourceIT {
             }
             """;
 
-    private static String SPECIFIC_QUESTION_URL = "/surveys/Survey1/questions/Question1";
-
-    //Used so that application directly talks with https://localhost:RANDOM_PORT
-    @Autowired
-    private TestRestTemplate template;
-
     @Test
-    void retrieveSpecificQuestionBySurveyIdAndQuestionId_basicScenario() {
-        ResponseEntity<String> responseEntity = template.getForEntity(SPECIFIC_QUESTION_URL, String.class);
-        System.out.println(responseEntity.getBody());
-        System.out.println(responseEntity.getHeaders());
+    void retrieveSpecificQuestionBySurveyIdAndQuestionId_basicScenario() throws JSONException {
+        ResponseEntity<String> responseEntity = template.getForEntity(GENERIC_QUESTIONS_URL, String.class);
+        String expectedResponse = """
+                [
+                     {
+                         "id": "Question1"
+                     },
+                     {
+                         "id": "Question2"
+                     },
+                     {
+                         "id": "Question3"
+                     }
+                ]
+                """;
+
+        // Status of the response is 200.
+        assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+
+        // Content-Type:"application/json"
+        assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
+
+        JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);
     }
 
+    @Test
+    void retrieveAllQuestionsBySurveyId_basicScenario() throws JSONException {
+        ResponseEntity<String> responseEntity = template.getForEntity(SPECIFIC_QUESTION_URL, String.class);
+        String expectedResponse = """
+                {
+                    "id":"Question1",
+                    "description":"Most Popular Cloud Platform Today",
+                    "correctAnswer":"AWS"
+                }
+                """;
+
+        // Status of the response is 200.
+        assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+
+        // Content-Type:"application/json"
+        assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
+
+        JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);
+    }
 }
