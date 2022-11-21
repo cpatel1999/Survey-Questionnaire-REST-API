b7a3b90830b03264b57ba324d4c563fc544db4f5
Charit_Patel <charit15499@gmail.com>
2022-11-21 15:06:52 -0500

    Mock MVC Unit Tests for GET method

--- src/main/java/com/application/springboot/restapi/survey/SurveyResource.java
+++ src/main/java/com/application/springboot/restapi/survey/SurveyResource.java
@@ -1,6 +1,4 @@
 package com.application.springboot.restapi.survey;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/test/java/com/application/springboot/restapi/survey/SurveyResourceTest.java
+++ src/test/java/com/application/springboot/restapi/survey/SurveyResourceTest.java
@@ -9,9 +9,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
@@ -26,6 +29,8 @@ public class SurveyResourceTest {
     private MockMvc mockMvc;
 
     private static String SPECIFIC_QUESTION_URL = "http://localhost:8080/surveys/Survey1/questions/Question1";
+    private static String GENERIC_QUESTIONS_URL = "http://localhost:8080/surveys/Survey1/questions";
+    private static String SPECIFIC_SURVEY_URI = "http://localhost:8080/surveys/Survey1";
 
     @Test
     void retrieveSpecificQuestionBySurveyIdAndQuestionId_404Scenario() throws Exception {
@@ -48,7 +53,9 @@ public class SurveyResourceTest {
         RequestBuilder requestBuilder = MockMvcRequestBuilders.get(SPECIFIC_QUESTION_URL)
                 .accept(MediaType.APPLICATION_JSON);
 
-        Question question = new Question("Question1", "Most Popular Cloud Platform Today", Arrays.asList("AWS", "Azure", "Google Cloud", "Oracle Cloud"), "AWS");
+        Question question = new Question("Question1",
+                "Most Popular Cloud Platform Today", Arrays.asList(
+                "AWS", "Azure", "Google Cloud", "Oracle Cloud"), "AWS");
 
         when(surveyService.retrieveSpecificQuestionBySurveyIdAndQuestionId("Survey1", "Question1"))
                 .thenReturn(question);
@@ -73,5 +80,79 @@ public class SurveyResourceTest {
 
         assertEquals(200, mvcResult.getResponse().getStatus());
         JSONAssert.assertEquals(expectedResponse, mvcResult.getResponse().getContentAsString(), false);
+    }
+
+    @Test
+    public void retrieveAllQuestionsBySurveyId_basicScenario() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(GENERIC_QUESTIONS_URL)
+                .accept(MediaType.APPLICATION_JSON);
+
+        Question question1 = new Question("Question1",
+                "Most Popular Cloud Platform Today", Arrays.asList(
+                "AWS", "Azure", "Google Cloud", "Oracle Cloud"), "AWS");
+        Question question2 = new Question("Question2",
+                "Fastest Growing Cloud Platform", Arrays.asList(
+                "AWS", "Azure", "Google Cloud", "Oracle Cloud"), "Google Cloud");
+        Question question3 = new Question("Question3",
+                "Most Popular DevOps Tool", Arrays.asList(
+                "Kubernetes", "Docker", "Terraform", "Azure DevOps"), "Kubernetes");
+
+        List<Question> questions = new ArrayList<>(Arrays.asList(question1,
+                question2, question3));
+
+        when(surveyService.retrieveAllQuestionsBySurveyId("Survey1")).thenReturn(questions);
+
+        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+
+        String expectedResponse = """
+                [
+                    {
+                        "id": "Question1"
+                    },
+                    {
+                        "id": "Question2"
+                    },
+                    {
+                        "id": "Question3"
+                    }
+                ]
+                """;
+        assertEquals(200, mvcResult.getResponse().getStatus());
+        JSONAssert.assertEquals(expectedResponse, mvcResult.getResponse().getContentAsString(), false);
+    }
+
+    @Test
+    public void retrieveSurveyById_basicScenario() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(SPECIFIC_SURVEY_URI)
+                .accept(MediaType.APPLICATION_JSON);
+
+        Question question1 = new Question("Question1",
+                "Most Popular Cloud Platform Today", Arrays.asList(
+                "AWS", "Azure", "Google Cloud", "Oracle Cloud"), "AWS");
+        Question question2 = new Question("Question2",
+                "Fastest Growing Cloud Platform", Arrays.asList(
+                "AWS", "Azure", "Google Cloud", "Oracle Cloud"), "Google Cloud");
+        Question question3 = new Question("Question3",
+                "Most Popular DevOps Tool", Arrays.asList(
+                "Kubernetes", "Docker", "Terraform", "Azure DevOps"), "Kubernetes");
+
+        List<Question> questions = new ArrayList<>(Arrays.asList(question1,
+                question2, question3));
+
+        Survey survey = new Survey("Survey1", "My Favorite Survey",
+                "Description of the Survey", questions);
+
+        when(surveyService.retrieveSurveyById("Survey1")).thenReturn(survey);
+
+        String expectedResponse = """
+                {
+                    "id": "Survey1"
+                }
+                """;
+
+        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+        assertEquals(200, mvcResult.getResponse().getStatus());
+        JSONAssert.assertEquals(expectedResponse, mvcResult.getResponse().getContentAsString(), false);
+        System.out.println(mvcResult.getResponse().getContentAsString());
     }
 }
